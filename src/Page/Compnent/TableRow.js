569c972b9ce4904c04668ff7953d7beecab64a79
unknown <abdulkaderjilani120@gmail.com>
2022-09-11 20:51:33 +0600

    order successfully send

--- src/Page/Compnent/TableRow.js
+++ src/Page/Compnent/TableRow.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
 import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const TableRow = ({ product, index }) => {
     const { name, img, price, color, size , _id } = product
     const [check , setCheck] = useState("")
-    const hanldleOrder =() =>{
-
+    const [inputQuantity, setInputQuantity] = useState(1)
+    
+    const handleCheck =(e) =>{
+          setCheck( e.target.value)
+       
+      
     }
-    console.log("check value" , check)
+    const hanldleOrder =() =>{
+        const mainPrice = inputQuantity * Number(price)       
+      const totalOrder ={
+        quantity : inputQuantity ,
+        img ,
+        name,
+        price : mainPrice
+
+      }
+      if(check == "on"){
+
+       axios.post("http://localhost:5000/fashion/order" , totalOrder)
+       .then(res => {       
+        toast("Thank you For order")
+     })
+        
+      }else{
+        alert("Please Fill check box . It does not complete your order")
+      }
+      
+    }
+   
     return (
         <tr>
             <th scope="row">{index + 1}</th>
@@ -19,11 +47,12 @@ const TableRow = ({ product, index }) => {
             <td className='text-success fw-bold'>In Stock</td>
             <td> $ {price}</td>
             <td>  {size}</td>
-            <td className='cart-td'>
-            <input className='quantity-input' placeholder='1' type="number" name="" id=""  min="1"/> 
-           <FontAwesomeIcon className='shoping-cart-icon' icon={ faCartShopping}  />
-             <input onChange={(e) => setCheck(e.target.value)} type="checkbox" name="" id="" />
+            <td className='cart-td d-md-flex  justify-content-center'>
+           <p>  <input onChange={(e) => setInputQuantity(e.target.value)} className='quantity-input' placeholder='1' type="number" name="" id=""  min="1"/> </p>
+            <p> <FontAwesomeIcon  onClick={hanldleOrder} className='shoping-cart-icon' icon={ faCartShopping}  /></p>
+            <p> <input onClick={handleCheck} type="checkbox" name="" id="" /></p>
             </td>
+            <ToastContainer />
         </tr>
     );
 };
